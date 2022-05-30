d87cfbaa5a03f4c7416dc90bee779273ab5d77d9
Thomas Neitmann <thomas.neitmann@roche.com>
2022-05-30 08:50:02 +0000

    version 0.2.0

--- R/data.R
+++ R/data.R
@@ -2,107 +2,158 @@
 #'
 #' A SDTM AE dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ae.xpt}
-"ae"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ae.xpt?raw=true}
+"admiral_ae"
 
 #' Concomitant Medication Dataset
 #'
 #' A SDTM CM dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/cm.xpt}
-"cm"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/cm.xpt?raw=true}
+"admiral_cm"
 
 #' Demography Dataset
 #'
 #' A SDTM DM dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt}
-"dm"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt?raw=true}
+"admiral_dm"
 
 #' Disposition Dataset
 #'
 #' A SDTM DS dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ds.xpt}
-"ds"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ds.xpt?raw=true}
+"admiral_ds"
 
 #' Electrocardiogram Dataset
 #'
 #' An example of standard SDTM EG dataset to be used in deriving ADEG dataset
 #'
-"eg"
+"admiral_eg"
 
 #' Exposure Dataset
 #'
 #' A SDTM EX dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt}
-"ex"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt?raw=true}
+"admiral_ex"
 
 #' Laboratory Measurements Dataset
 #'
 #' A SDTM LB dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/lb.xpt}
-"lb"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/lb.xpt?raw=true}
+"admiral_lb"
 
 #' Medical History Dataset
 #'
 #' A SDTM MH dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/mh.xpt}
-"mh"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/mh.xpt?raw=true}
+"admiral_mh"
+
+#' Pharmacokinetics Concentrations Dataset
+#'
+#' A SDTM PC dataset simulated by Antonio Rodríguez Contestí
+#'
+#' @source \url{https://github.com/pharmaverse/admiral.test/blob/main/data/admiral_pc.rda}
+#' @author Antonio Rodríguez Contestí
+"admiral_pc"
+
+#' Pharmacokinetic Parameters Dataset
+#'
+#' A SDTM PP dataset simulated by Antonio Rodríguez Contestí
+#'
+#' @source \url{https://github.com/pharmaverse/admiral.test/blob/main/data/admiral_pp.rda}
+#' @author Antonio Rodríguez Contestí
+"admiral_pp"
 
 #' Questionnaire Dataset
 #'
 #' A SDTM QS dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/qs.xpt}
-"qs"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/qs.xpt?raw=true}
+"admiral_qs"
+
+#' Tumor Identification Dataset
+#'
+#' A SDTM TU dataset simulated by Gopi Vegesna
+#'
+#' @author Gopi Vegesna
+"admiral_tu"
+
+#' Tumor Results Dataset
+#'
+#' A SDTM TR dataset simulated by Gopi Vegesna
+#'
+#' @author Gopi Vegesna
+"admiral_tr"
+
+#' Disease Response Dataset
+#'
+#' A SDTM RS dataset simulated by Gopi Vegesna
+#'
+#' @author Gopi Vegesna
+"admiral_rs"
 
 #' Supplemental Adverse Events Dataset
 #'
 #' A SDTM SUPPAE dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppae.xpt}
-"suppae"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppae.xpt?raw=true}
+"admiral_suppae"
 
 #' Supplemental Disposition Dataset
 #'
 #' A SDTM SUPPDS dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppds.xpt}
-"suppds"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppds.xpt?raw=true}
+"admiral_suppds"
 
 #' Supplemental Demography Dataset
 #'
 #' A SDTM SUPPDM dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppdm.xpt}
-"suppdm"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/suppdm.xpt?raw=true}
+"admiral_suppdm"
+
+#' Supplemental Tumor Results Dataset
+#'
+#' A SDTM SUPPTR dataset simulated by Gopi Vegesna
+#'
+#' @author Gopi Vegesna
+"admiral_supptr"
 
 #' Trial Design Dataset
 #'
 #' A SDTM TS dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ts.xpt}
-"ts"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ts.xpt?raw=true}
+"admiral_ts"
 
 #' Vital Signs Dataset
 #'
 #' A SDTM VS dataset from the CDISC pilot project
 #'
-#' @source \url{https://bitbucket.cdisc.org/projects/CED/repos/sdtm-adam-pilot-project/browse/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/vs.xpt}
-"vs"
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/vs.xpt?raw=true}
+"admiral_vs"
 
 #' SMQ Dataset
 #'
 #' An example SMQ dataset
 #'
-"smq_db"
+"admiral_smq_db"
 
 #' SDG Dataset
 #'
 #' An example SDG dataset
 #'
-"sdg_db"
+"admiral_sdg_db"
+
+#' Subject Visits Dataset
+#'
+#' A SDTM SV dataset from the CDISC pilot project
+#'
+#' @source \url{https://github.com/cdisc-org/sdtm-adam-pilot-project/blob/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/sv.xpt?raw=true}
+"admiral_sv"
